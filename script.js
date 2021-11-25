76c745ebe71cf4f184576bb59aca88fe91770e33
Hannah Dalwai <hannahdalwai4@gmail.com>
2021-11-25 12:05:20 +0200

    notSure

--- script.js
+++ script.js
@@ -3,20 +3,20 @@ let sum = "";
 function addToSum(operator){
     sum = sum + operator;
     console.log(sum);
-    document.getElementById("output").innertext = sum;
+    document.getElementById("output").innerText = sum;
 }
 
 function evaluateSum(){
     sum = eval(sum);
-    document.getElementById("output").innertext = sum;
+    document.getElementById("output").innerText = sum;
 }
 
 function clearSum(){
 sum = "";
-document.getElementById("output").innertext = sum;
+document.getElementById("output").innerText = sum;
 }
 
 function deleteChar(){
     sum = sum.slice(0, -1);
-    document.getElementById("output").innertext = sum;
+    document.getElementById("output").innerText = sum;
 }
